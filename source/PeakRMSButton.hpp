c3439a2e1c7f64ff6c5166c2c2ab5ec9c3bdca8d
Daniel Rudrich <mail@danielrudrich.de>
2022-03-05 15:13:25 +0100

    adjusts rms button layout

--- source/PeakRMSButton.hpp
+++ source/PeakRMSButton.hpp
@@ -18,8 +18,10 @@ public:
         peak.cubicTo ({54.0f, 0.0f},  {68.0f, 34.0f}, {68.0f, 34.0f});
 
         rms.startNewSubPath ({0.0f, 34.0f});
-        rms.cubicTo ({14.0f, 34.0f},  {25.0f, 0.0f}, {34.0f, 0.0f});
-        rms.cubicTo ({49.0f, 0.0f}, {54.0f, 34.0f}, {68.0f, 34.0f});
+        rms.cubicTo ({18.0f, 34.0f},  {25.0f, 0.0f}, {34.0f, 0.0f});
+        rms.cubicTo ({49.0f, 0.0f}, {50.0f, 34.0f}, {68.0f, 34.0f});
+
+        setRepaintsOnMouseActivity (true);
     }
 
     ~PeakRMSButton() override = default;
@@ -27,11 +29,16 @@ public:
     void paint (juce::Graphics& g) override
     {
         auto activeColour = juce::Colour (0xFF9013FE);
-        auto inactiveColour = juce::Colour (0xFF414141);
+        auto inactiveColour = juce::Colour (0xFF414141).withAlpha (isMouseOver() ? 1.0f : 0.5f);
 
-        g.fillAll (activeColour.withAlpha (0.14f));
+        auto bounds = getLocalBounds();
+        auto textBounds = bounds.reduced (5, 4).toFloat();
+        auto graphicBounds = bounds.removeFromTop (bounds.getHeight() / 2).toFloat();
 
-        auto bounds = getLocalBounds().reduced (5, 4).toFloat();
+        g.setColour (activeColour.withAlpha (0.14f));
+        g.fillRect (graphicBounds);
+
+        graphicBounds.reduce (4, 2);
 
         juce::String parameterLabel;
         juce::Path* activePath;
@@ -50,15 +57,15 @@ public:
         }
 
         g.setColour (inactiveColour);
-        g.strokePath (*inactivePath, juce::PathStrokeType (1.0f), inactivePath->getTransformToScaleToFit (bounds, false));
+        g.strokePath (*inactivePath, juce::PathStrokeType (1.0f), inactivePath->getTransformToScaleToFit (graphicBounds, false));
 
         g.setColour (activeColour);
-        g.strokePath (*activePath, juce::PathStrokeType (3.0f), activePath->getTransformToScaleToFit (bounds, false));
+        g.strokePath (*activePath, juce::PathStrokeType (3.0f), activePath->getTransformToScaleToFit (graphicBounds, false));
 
         g.setFont (Fonts::getRegularFont());
         g.setFont (15);
         g.setColour (juce::Colours::black);
-        g.drawText (parameterLabel, bounds, juce::Justification::centredBottom);
+        g.drawText (parameterLabel, textBounds, juce::Justification::centredBottom);
     }
 
     void mouseUp ([[maybe_unused]]const juce::MouseEvent& event) override
